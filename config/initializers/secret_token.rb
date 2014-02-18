cc8231e21233af50a0be87c8ed2bc6dd6fd2d724
Javier CRG <alu0100505023@ull.edu.es>
2014-02-18 10:25:00 +0000

    Iniciando proyecto en Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pract2Rails::Application.config.secret_key_base = 'bb9cd9416023b0279208c94188fb0bfbf1fa34ce9c722b2c6abb95be41fd15f6556f2502d4b14d49cb128ef49fee46b9d1d5c45d271f2c66dc62bc19c5dd1a58'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pract2Rails::Application.config.session_store :cookie_store, key: '_pract2-rails_session'
